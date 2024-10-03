e61603d17cff7c41840c36af0296278a769e9747
Arman Shaikh <arman.shaikh.work@gmail.com>
2024-10-03 14:11:25 +0100

    adding doctring

--- app.py
+++ app.py
@@ -1,33 +1,57 @@
-from flask import Flask, render_template, session, redirect
+"""
+This module initializes a Flask application, connects to a MongoDB database, and defines routes with login-required decorators.
+Modules:
+    flask: A micro web framework for Python.
+    functools: Higher-order functions and operations on callable objects.
+    pymongo: Python driver for MongoDB.
+    dotenv: Reads key-value pairs from a .env file and can set them as environment variables.
+    os: Provides a way of using operating system dependent functionality.
+    user.routes: Custom module for user-related routes.
+Functions:
+    login_required(f): A decorator to ensure that a user is logged in before accessing certain routes.
+Routes:
+    /: The home route which requires the user to be logged in and renders the 'home.html' template.
+"""
+import os
 from functools import wraps
 import pymongo
 from dotenv import load_dotenv
-import os
+from flask import Flask, render_template, session, redirect
+
+
 load_dotenv()
  
 app = Flask(__name__)
 
 client = pymongo.MongoClient(os.getenv(("DB_LOGIN")))
-from user import routes
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+except pymongo.errors.ConfigurationError as e:
+    print(f"Configuration error: {e}")
+except pymongo.errors.OperationFailure as e:
+    print(f"Operation failure: {e}")
 
 # Decorators
 def login_required(f):
-  @wraps(f)
-  def wrap(*args, **kwargs):
-    if 'logged_in' in session:
-      return f(*args, **kwargs)
-    else:
-      return redirect('/user/login')
+    """This decorator ensures that a user is logged in before accessing certain routes.
+    """
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect('/user/login')
   
-  return wrap
+    return wrap
     
 @app.route('/')
 @login_required
 def index():
+    """The home route which requires the user to be logged in and renders the 'home.html' template.
+
+    Returns:
+        str: Rendered HTML template for the home page.
+    """
     return render_template('home.html')
  
--- user/routes.py
+++ user/routes.py
@@ -1,19 +1,32 @@
-from flask import Flask, render_template
+"""
+This module defines the routes for user-related actions in the Flask application.
+
+Routes:
+    /user/register (POST): Registers a new user.
+    /user/signout: Signs out the current user.
+    /user/login: Renders the login page.
+    /user/login_attempt (POST): Attempts to log in a user.
+"""
+from flask import render_template
 from app import app
 from user.models import User
 
 @app.route('/user/register', methods=['POST'])
 def signup():
-  return User().register()
+    """Registers a new user."""
+    return User().register()
 
 @app.route('/user/signout')
 def signout():
-  return User().signout()
+    """Signs out the current user."""
+    return User().signout()
 
 @app.route('/user/login')
 def login():
-  return render_template('login.html')
+    """Gives login form to user."""
+    return render_template('login.html')
 
 @app.route('/user/login_attempt', methods=['POST'])
 def login_attempt():
-  return User().login()
+    """Processes login form."""
+    return User().login()
