d26a6f49df99764145f8a0d1e5c705d9260f0366
Arman Shaikh <arman.shaikh.work@gmail.com>
2024-10-04 12:12:08 +0100

    added tests for database

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,12 +1,9 @@
 from flask import Flask, render_template
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import pytest
-import pymongo
-from core.database import client
+from ..core import database
 from _pytest.monkeypatch import MonkeyPatch
+import uuid
 
 @pytest.fixture
 def monkeypatch():
@@ -18,7 +15,38 @@ def monkeypatch():
 
 def test_ping_success():
     try:
-        client.admin.command('ping')
+        database.client.admin.command('ping')
         assert True, "Pinged your deployment. You successfully connected to MongoDB!"
     except Exception as e:
         pytest.fail(f"Unexpected error: {e}")
+        
+def test_find_dummy_login():
+    user = database.users_collection.find_one({"email": "dummy@dummy.com"})
+    
+    if user:
+        assert user['email'] == "dummy@dummy.com"
+        assert user['password'] == "dummy"
+        assert user['name'] == "dummy"
+    else:
+        pytest.fail("User not found")
+        
+def test_add_and_remove_dummy_login():
+    user = {
+        "_id": uuid.uuid4().hex,
+        "name": "dummy2",
+        "email": "dummy2@dummy.com",
+        "password": "dummy2"
+    }
+    database.users_collection.insert_one(user)
+    
+    # Assert the user was added
+    added_user = database.users_collection.find_one({"email": "dummy2@dummy.com"})
+    assert added_user is not None, "User was not added"
+    assert added_user['email'] == "dummy2@dummy.com"
+
+    # Delete the user
+    database.users_collection.delete_one({"email": "dummy2@dummy.com"})
+
+    # Confirm the user was deleted
+    deleted_user = database.users_collection.find_one({"email": "dummy2@dummy.com"})
+    assert deleted_user is None, "User was not deleted"
