a2534d334e29c4881c4c5a65ee0e15e15e30ffb8
Arman Shaikh <arman.shaikh.work@gmail.com>
2024-10-08 13:08:33 +0100

    Refactor User class and improve login functionality

--- user/models.py
+++ user/models.py
@@ -2,10 +2,10 @@
 """
 This module defines the User class which handles user authentication and session management.
 """
-from flask import jsonify, request, session, redirect
-from core import database
-from passlib.hash import pbkdf2_sha256
 import uuid
+from flask import jsonify, request, session, redirect
+from passlib.hash import pbkdf2_sha256
+from core import database
 
 class User:
     """A class used to represent a User and handle user-related operations 
@@ -17,7 +17,7 @@ class User:
         session['logged_in'] = True
         session['user'] = user
         return jsonify(user), 200
-    
+
     def register(self):
         """Registers a new user by creating a user dictionary with a unique ID, 
         name, email, and password, and returns a JSON response indicating failure."""
@@ -34,19 +34,19 @@ class User:
             "email": request.form.get('email'),
             "password": pbkdf2_sha256.hash(password)  # Hash only the password
         }
-        
+
         if database.users_collection.find_one({"email": request.form.get('email')}):
             return jsonify({"error": "Email address already in use"}), 400
-        
+
         # Insert the user into the database
         database.users_collection.insert_one(user)
 
         # Start session or return success response
         if user:
             return self.start_session(user)
-    
+
         return jsonify({"error": "Signup failed"}), 400
-    
+
     def signout(self):
         """Clears the current session and redirects to the home page."""
         session.clear()
@@ -57,8 +57,9 @@ class User:
         invalid login credentials."""
         session.clear()
         user = database.users_collection.find_one({"email": request.form.get("email")})
-        
+
         if user and pbkdf2_sha256.verify(request.form.get('password'), user['password']):
             return self.start_session(user)
 
         return jsonify({"error": "Invalid login credentials"}), 401
+    
