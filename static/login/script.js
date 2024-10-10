985aed873bdf6526a002155db8f05479c0b8e682
Yousef Abdalla <u03ya23@abdn.ac.uk>
2024-10-10 12:55:02 +0100

    trial

--- static/login/script.js
+++ static/login/script.js
@@ -11,20 +11,20 @@ document.addEventListener("DOMContentLoaded", () => {
             method: "POST",
             body: formData,
         })
-            .then(async (response) => {
-                if (response.ok) {
-                    window.location.href = "/"; // Redirect on successful login
-                } else if (response.status === 401 || response.status === 400) {
-                    let errorResponse = await response.json(); // Parse JSON response
-                    throw new Error(errorResponse.error); // Throw error with the extracted message
-                } else {
-                    throw new Error("Server error"); // General server error message
-                }
-            })
-            .catch((error) => {
-                console.error("Error:", error); // Log error to console
-                error_paragraph.textContent = error.message; // Use `error.message` to display the error
-                error_paragraph.classList.remove("error--hidden"); // Ensure the error paragraph is visible
-            });
+        .then(async (response) => {
+            if (response.ok) {
+                window.location.href = "/"; // Redirect on successful login
+            } else if (response.status === 401 || response.status === 400) {
+                let errorResponse = await response.json(); // Parse JSON response
+                throw new Error(errorResponse.error); // Throw error with the extracted message
+            } else {
+                throw new Error("Server error"); // General server error message
+            }
+        })
+        .catch((error) => {
+            console.error("Error:", error); // Log error to console
+            error_paragraph.textContent = error.message; // Display the error message
+            error_paragraph.classList.remove("error--hidden"); // Ensure the error paragraph is visible
+        });
     });
 });
