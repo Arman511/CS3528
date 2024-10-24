23b23973c862fe29b60dd37468304040d0fd18c9
Arman Shaikh <arman.shaikh.work@gmail.com>
2024-10-24 12:52:32 +0100

    Refactor test_conf.py: Remove unnecessary assertion

--- tests/test_database.py
+++ tests/test_database.py
@@ -9,17 +9,7 @@ def test_ping_success():
         assert True, "Pinged your deployment. You successfully connected to MongoDB!"
     except Exception as e:
         pytest.fail(f"Unexpected error: {e}")
-        
-def test_find_dummy_login():
-    user = database.users_collection.find_one({"email": "dummy@dummy.com"})
-    
-    if user:
-        assert user['email'] == "dummy@dummy.com"
-        assert user['password'] == "dummy"
-        assert user['name'] == "dummy"
-    else:
-        pytest.fail("User not found")
-        
+
 def test_add_and_remove_dummy_login():
     user = {
         "_id": uuid.uuid4().hex,
