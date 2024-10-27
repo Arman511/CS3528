6d51940324f513affc2c60a8260522ea03023462
Arman Shaikh <arman.shaikh.work@gmail.com>
2024-10-27 23:17:47 +0000

    Refactor test_student.py and import necessary modules

--- tests/test_student.py
+++ tests/test_student.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
-
+import pytest
+from flask import Flask, session
+from flask.testing import FlaskClient
 from ..app import app
+from ..students.models import Student
 
 
-def test_create_app():
-    pass
