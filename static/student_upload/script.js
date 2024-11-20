6b2bbd00ec56cbf5061906418b6dfa1527596976
Arman Shaikh <arman.shaikh.work@gmail.com>
2024-11-20 09:45:08 +0000

    Fixed upload

--- static/student_upload/script.js
+++ static/student_upload/script.js
@@ -1,32 +1,38 @@
-const csvForm = document.forms["upload_csv_form"];
-const xlsForm = document.forms["upload_xls_form"];
+document.addEventListener("DOMContentLoaded", function () {
+    const csvForm = document.forms["upload_csv_form"];
+    const xlsForm = document.forms["upload_xls_form"];
 
-csvForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    uploadFile(csvForm, "/students/upload_csv");
-});
-
-xlsForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    uploadFile(xlsForm, "/students/upload_xlsx");
-});
-
-function uploadFile(form, url) {
-    const formData = new FormData(form);
-    fetch(url, {
-        method: "POST",
-        body: formData,
-    })
-        .then((response) => {
-            if (response.ok) {
-                alert(response.text());
-            } else {
-                alert("Error: " + response.json().data.error);
-            }
-        })
-        .then((response) => response.json())
-        .catch((error) => {
-            console.error("Error:", error);
-            alert("An error occurred while uploading the file");
+    csvForm.addEventListener("submit", async function (event) {
+        event.preventDefault();
+        const formData = new FormData(csvForm);
+        const response = await fetch("/students/upload_csv", {
+            method: "POST",
+            body: formData,
         });
-}
+
+        if (response.ok) {
+            const data = await response.json();
+            alert(data.message);
+        } else {
+            console.log("Error");
+            alert("Error: " + response.json().error);
+        }
+    });
+
+    xlsForm.addEventListener("submit", async function (event) {
+        event.preventDefault();
+        const formData = new FormData(xlsForm);
+        const response = await fetch("/students/upload_xlsx", {
+            method: "POST",
+            body: formData,
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            alert(data.message);
+        } else {
+            console.log("Error");
+            alert("Error: " + response.json().error);
+        }
+    });
+});
