6ec3286e38908798d10f810120ed46abe4b8d104
Arman Shaikh <arman.shaikh.work@gmail.com>
2025-02-04 20:51:43 +0000

    Add test fixtures for client and database in user tests

--- tests/route_tests/test_user.py
+++ tests/route_tests/test_user.py
@@ -3,11 +3,58 @@
 # pylint: disable=redefined-outer-name
 # flake8: noqa: F811
 
-from ..test_conf import (  # pylint: disable=unused-import
-    database,
-    client,
-    user_logged_in_client,
-)
+import os
+import uuid
+from pymongo import MongoClient
+from passlib.hash import pbkdf2_sha256
+import pytest
+
+
+
+@pytest.fixture()
+def client():
+    """Fixture to create a test client."""
+    from ...app import app  # pylint: disable=import-outside-toplevel
+
+    return app.test_client()
+
+
+@pytest.fixture()
+def database():
+    """Fixture to create a test database."""
+    connection = MongoClient(os.getenv("MONGO_URI"))
+    database = connection[os.getenv("MONGO_DB_TEST", "client_test")]
+
+    return database
+
+
+@pytest.fixture()
+def user_logged_in_client(client, database):
+    """Fixture to login a user."""
+    user_collection = database["users"]
+    user_collection.delete_many({"email": "dummy@dummy.com"})
+    user = {
+        "_id": uuid.uuid4().hex,
+        "name": "dummy",
+        "email": "dummy@dummy.com",
+        "password": pbkdf2_sha256.hash("dummy"),
+    }
+
+    user_collection.insert_one(user)
+
+    url = "/user/login"
+    client.post(
+        url,
+        data={
+            "email": "dummy@dummy.com",
+            "password": "dummy",
+        },
+    )
+
+    yield client
+
+    # Cleanup code
+    user_collection.delete_many({"email": "dummy@dummy.com"})
 
 
 def test_upload_student_page(user_logged_in_client):
