4959b9f1076304c878792adfb2d03ad323c7167c
Arman Shaikh <arman.shaikh.work@gmail.com>
2025-02-10 20:03:43 +0000

    Refactor configuration management and update skill ranking logic

--- app.py
+++ app.py
@@ -11,11 +11,13 @@ from flask import Flask
 from flask_caching import Cache
 
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+from core.configuration_settings import Config  # noqa: E402
 from core.database_mongo_manager import DatabaseMongoManager  # noqa: E402
 from core import handlers  # noqa: E402
 
 DATABASE_MANAGER = None
 DEADLINE_MANAGER = None
+CONFIG_MANAGER = None
 
 load_dotenv()
 
@@ -45,6 +47,8 @@ tables = [
 
 for table in tables:
     DATABASE_MANAGER.add_table(table)
+
+CONFIG_MANAGER = Config()
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
--- core/configuration_settings.py
+++ core/configuration_settings.py
@@ -8,22 +8,18 @@ class Config:
         from app import DATABASE_MANAGER
 
         self.database_manager = DATABASE_MANAGER
-        self.num_of_skills = 10
+        self.max_num_of_skills = 10
         self.min_num_ranking_student_to_opportunities = 5
-        self.min_num_ranking_employer_to_students = 5
 
         self.update()
 
-    def get_num_of_skills(self):
+    def get_max_num_of_skills(self) -> int:
         """Get number of skills"""
-        return self.num_of_skills
+        return self.max_num_of_skills
 
-    def get_min_num_ranking_student_to_opportunities(self):
-
+    def get_min_num_ranking_student_to_opportunities(self) -> int:
+        """Get minimum number of ranking student to opportunities"""
         return self.min_num_ranking_student_to_opportunities
-
-    def get_min_num_ranking_employer_to_students(self):
-        return self.min_num_ranking_employer_to_students
 
     def update(self):
         temp_num_of_skills = self.database_manager.get_one_by_field(
@@ -34,23 +30,25 @@ class Config:
                 "config", "name", "min_num_ranking_student_to_opportunities"
             )
         )
-        temp_min_num_ranking_employer_to_students = (
-            self.database_manager.get_one_by_field(
-                "config", "name", "min_num_ranking_employer_to_students"
-            )
-        )
 
         if temp_num_of_skills:
-            self.num_of_skills = temp_num_of_skills["value"]
+            self.max_num_of_skills = temp_num_of_skills["value"]
+        else:
+            self.database_manager.insert(
+                "config", {"name": "num_of_skills", "value": self.max_num_of_skills}
+            )
 
         if temp_min_num_ranking_student_to_opportunities:
             self.min_num_ranking_student_to_opportunities = (
                 temp_min_num_ranking_student_to_opportunities["value"]
             )
-
-        if temp_min_num_ranking_employer_to_students:
-            self.min_num_ranking_employer_to_students = (
-                temp_min_num_ranking_employer_to_students["value"]
+        else:
+            self.database_manager.insert(
+                "config",
+                {
+                    "name": "min_num_ranking_student_to_opportunities",
+                    "value": self.min_num_ranking_student_to_opportunities,
+                },
             )
 
     def set_num_of_skills(self, num_of_skills):
@@ -68,16 +66,5 @@ class Config:
             "name",
             "min_num_ranking_student_to_opportunities",
             {"value": min_num_ranking_student_to_opportunities},
-        )
-        self.update()
-
-    def set_min_num_ranking_employer_to_students(
-        self, min_num_ranking_employer_to_students
-    ):
-        self.database_manager.update_one_by_field(
-            "config",
-            "name",
-            "min_num_ranking_employer_to_students",
-            {"value": min_num_ranking_employer_to_students},
         )
         self.update()
