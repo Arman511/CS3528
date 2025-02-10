b25384f1aadb660cae2ed15736772016ec6c187d
Yousef Abdalla <u03ya23@abdn.ac.uk>
2025-02-10 10:08:04 +0000

    getting started on the employer routes testing

---
+++ tests/route_tests/test_employer.py
@@ -0,0 +1,71 @@
+""" Test for the employer routes."""
+
+# pylint: disable=redefined-outer-name
+# flake8: noqa: F811
+
+import os
+import sys
+import uuid
+
+# Add the root directory to the Python path
+sys.path.append(
+    os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+)
+from passlib.hash import pbkdf2_sha256
+import pytest
+from dotenv import load_dotenv
+
+from core.database_mongo_manager import DatabaseMongoManager
+
+os.environ["IS_TEST"] = "True"
+
+load_dotenv()
+
+
+@pytest.fixture()
+def client():
+    """Fixture to create a test client."""
+    from ...app import app  # pylint: disable=import-outside-toplevel
+
+    app.config["TESTING"] = True
+    return app.test_client()
+
+
+@pytest.fixture()
+def database():
+    """Fixture to create a test database."""
+
+    DATABASE = DatabaseMongoManager(
+        os.getenv("MONGO_URI"), os.getenv("MONGO_DB_TEST", "cs3528_testing")
+    )
+
+    yield DATABASE
+
+    # Cleanup code
+    DATABASE.connection.close()
+
+
+def employer_logged_in_client(client, database: DatabaseMongoManager):
+    """Fixture to login an employer."""
+    database.add_table("employers")
+    database.delete_all_by_field("employers", "email", "dummy@dummy.com")
+
+    employer = {
+        "_id": uuid.uuid4().hex,
+        "company_name": "dummy",
+        "email": "dummy@dummy.com",
+        "password": pbkdf2_sha256.hash("dummy"),
+    }
+
+    database.insert_one("employers", employer)
+
+    url = "/employers/login"
+    client.post(
+        url,
+        data={"email": "dummy@dummy.com"},
+        content_type="application/x-www-form-urlencoded",
+    )
+    
+    yield client
+    
+    database.delete_all_by_field("employers", "email", "dummy@dummy.com")
