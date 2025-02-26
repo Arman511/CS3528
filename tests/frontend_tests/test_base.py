09ffaf71be0c93317694926a3781c77b936c2d1c
Arman Shaikh <arman.shaikh.work@gmail.com>
2025-02-26 18:01:41 +0000

    Enhance employer login flow and add employer fixture for testing

--- tests/frontend_tests/test_base.py
+++ tests/frontend_tests/test_base.py
@@ -27,13 +27,15 @@ os.environ["IS_TEST"] = "True"
 
 load_dotenv()
 
+PORT = 5000
+
 
 @pytest.fixture()
 def chrome_browser():
     options = Options()
-    # options.add_argument("--headless")  # Run Chrome in headless mode
-    # options.add_argument("--no-sandbox")
-    # options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--headless")  # Run Chrome in headless mode
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(options=options)
 
@@ -53,6 +55,14 @@ def flask_server():
     # Set up logging to print to console
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
+    # Kill any process running on port 5000
+    if os.name == "nt":
+        os.system(
+            "netstat -ano | findstr :5000 | findstr LISTENING | for /F \"tokens=5\" %P in ('more') do TaskKill /F /PID %P"
+        )
+    else:
+        os.system("fuser -k 5000/tcp")
+
     server_thread = threading.Thread(
         target=lambda: app.run(port=5000, debug=False, use_reloader=False)
     )
@@ -60,7 +70,6 @@ def flask_server():
     server_thread.start()
     yield
     # After the yield, kill the app
-    server_thread.join()
     if server_thread.is_alive():
         import signal
 
@@ -68,6 +77,14 @@ def flask_server():
             os.kill(os.getpid(), signal.SIGINT)
         except Exception as e:
             print(e)
+    server_thread.join()
+
+    if os.name == "nt":
+        os.system(
+            "netstat -ano | findstr :5000 | findstr LISTENING | for /F \"tokens=5\" %P in ('more') do TaskKill /F /PID %P"
+        )
+    else:
+        os.system("fuser -k 5000/tcp")
 
 
 @pytest.fixture()
@@ -236,6 +253,25 @@ def student_member(database):
         database.insert("courses", course)
 
 
+@pytest.fixture
+def employer_member(database):
+    employers = database.get_all("employers")
+    database.delete_all("employers")
+
+    employer = {
+        "_id": uuid.uuid4().hex,
+        "company_name": "dummy",
+        "email": "dummy@dummy.com",
+    }
+
+    database.insert("employers", employer)
+    yield employer
+
+    database.delete_all("employers")
+    for employer in employers:
+        database.insert("employers", employer)
+
+
 def test_placement_team_login(chrome_browser, flask_server, database, placement_member):
     chrome_browser.get("http://localhost:5000/user/login")
     chrome_browser.find_element(By.NAME, "email").send_keys("dummy@dummy.com")
@@ -269,7 +305,33 @@ def test_student_login(chrome_browser, flask_server, database, student_member):
     chrome_browser.find_element(By.ID, "optSubmit").click()
 
     WebDriverWait(chrome_browser, 10).until(
-        EC.invisibility_of_element_located((By.ID, "otpModal"))
+        EC.url_changes("http://localhost:5000/students/login")
     )
 
     assert chrome_browser.current_url != "http://localhost:5000/students/login"
+
+
+def test_employer_login(chrome_browser, flask_server, database, employer_member):
+    chrome_browser.get("http://localhost:5000/employers/login")
+    chrome_browser.find_element(By.NAME, "email").send_keys("dummy@dummy.com")
+    chrome_browser.find_element(By.CSS_SELECTOR, "input[type='submit']").click()
+    WebDriverWait(chrome_browser, 10).until(
+        EC.visibility_of_element_located((By.ID, "otpModal"))
+    )
+
+    session_content = chrome_browser.execute_script(
+        "return fetch('/debug/session').then(res => res.json());"
+    )
+    serialised_otp = session_content["OTP"]
+    secret_key = os.getenv("SECRET_KEY", "secret")
+    serializer = URLSafeSerializer(secret_key)
+
+    otp = serializer.loads(serialised_otp)
+    chrome_browser.find_element(By.ID, "otpInput").send_keys(otp)
+    chrome_browser.find_element(By.ID, "optSubmit").click()
+
+    WebDriverWait(chrome_browser, 10).until(
+        EC.invisibility_of_element_located((By.ID, "otpModal"))
+    )
+
+    assert chrome_browser.current_url == "http://localhost:5000/employers/home"
