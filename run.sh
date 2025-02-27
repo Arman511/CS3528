7aa2a27e30d57ddf2e1310fd8c399871f98811ff
Arman Shaikh <arman.shaikh.work@gmail.com>
2025-02-27 12:49:26 +0000

    Add OFFLINE environment variable to run script for better configuration

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-FLASK_APP=app.py FLASK_ENV="development" FLASK_DEBUG=1 IS_TEST="False" flask run
+FLASK_APP=app.py FLASK_ENV="development" FLASK_DEBUG=1 IS_TEST="False" OFFLINE="False" flask run
