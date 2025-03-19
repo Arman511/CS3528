c806768e7edd239e8911a911acacf8b56257c5f6
Arman Shaikh <arman.shaikh.work@gmail.com>
2025-03-19 19:16:05 +0000

    Refactor login form handling to improve error messaging and streamline submission process

--- static/login/script.js
+++ static/login/script.js
@@ -1,11 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let form = document.querySelector(".login_form");
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+    let submit = document.querySelector("#submit");
+
     let error_paragraph = document.querySelector(".error");
 
-    // Form submission logic
-    form.addEventListener("submit", async function (e) {
-        e.preventDefault(); // Prevent form submission
-        let formData = new FormData(form); // Collect form data
+    submit.addEventListener("click", async function (event) {
+        event.preventDefault(); // Prevent default form submission
+        let formData = new FormData();
+        formData.set("email", email.value);
+        formData.set("password", password.value);
+
+        if (email.value === "" || password.value === "") {
+            error_paragraph.textContent = "Please fill in all fields";
+            error_paragraph.classList.remove("error--hidden");
+        }
+
+        if (email.value !== "" && password.value !== "") {
+            error_paragraph.classList.add("error--hidden");
+        }
 
         try {
             const response = await fetch("/user/login", {
@@ -18,16 +31,18 @@ document.addEventListener("DOMContentLoaded", function () {
                 window.location.href = data.message;
             } else if (response.status === 401 || response.status === 400) {
                 let errorResponse = await response.json(); // Parse JSON response
+                fetch("/signout");
                 throw new Error(errorResponse.error); // Throw error with the extracted message
             } else {
+                fetch("/signout");
                 throw new Error("Server error"); // General server error message
             }
         } catch (error) {
             console.error("Error:", error); // Log error to console
             error_paragraph.textContent = error.message; // Use `error.message` to display the error
             error_paragraph.classList.remove("error--hidden"); // Ensure the error paragraph is visible
-        }
 
+        }
     });
     document.title = "SkillPilot - Placement Team Login";
 });
